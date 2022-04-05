e1c59f078cb9655112419e6aa6c9ddb44262c6ae
Sreenath285 <sreenath.285@gmail.com>
2022-04-05 16:13:29 +0530

    Sorting person by name

---
+++ AddressBook/src/main/java/MultipleAddressBook.java
@@ -0,0 +1,124 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class MultipleAddressBook {
+
+    Map<String, AddressBookService> multipleAddressBookMap = new HashMap<>();
+    Map<String, Person> personMap = new HashMap<String, Person>();
+    Scanner sc = new Scanner(System.in);
+
+    /***
+     * created addAddressBook method to add a new address book
+     */
+    public void addAddressBook () {
+        System.out.print("Enter address book name : ");
+        String addressBookName = sc.next();
+        if (multipleAddressBookMap.containsKey(addressBookName)) {
+            System.out.println("Address book already exists");
+        }
+        else {
+            AddressBookService addressBookService = new AddressBookService();
+            multipleAddressBookMap.put(addressBookName, addressBookService);
+            multipleAddressBookMap.get(addressBookName).getList();
+        }
+    }
+
+    /***
+     * created existingAddressBook method to get existing address book
+     */
+    public void existingAddressBook() {
+        System.out.print("Enter address book name : ");
+        String existingAddressBookName = sc.next();
+        if (multipleAddressBookMap.get(existingAddressBookName) == null) {
+            System.out.println("There is no address book with the given name");
+        }
+        else {
+            multipleAddressBookMap.get(existingAddressBookName).getList();
+        }
+    }
+
+    /***
+     * created showAddressBook method to show address book contacts
+     */
+    public void showAddressBook() {
+        for (String name : multipleAddressBookMap.keySet()) {
+            System.out.println(name);
+            System.out.println(multipleAddressBookMap.get(name).personList);
+        }
+    }
+
+    /***
+     * created searchByCity method to search person by city
+     */
+    public void searchByCity() {
+        System.out.print("Enter city to search person : ");
+        String cityName = sc.next();
+        for (String i : multipleAddressBookMap.keySet()) {
+            List<Person> personList = multipleAddressBookMap.get(i).personList;
+            personList.stream()
+                      .filter(person -> person.getCity().equals(cityName))
+                      .forEach(person -> System.out.println(person.getFirstName()));
+        }
+    }
+
+    /***
+     * created searchByState method to search person by state
+     */
+    public void searchByState() {
+        System.out.print("Enter state to search person : ");
+        String state = sc.next();
+        for (String i : multipleAddressBookMap.keySet()) {
+            List<Person> personList = multipleAddressBookMap.get(i).personList;
+            personList.stream()
+                    .filter(person -> person.getCity().equals(state))
+                    .forEach(person -> System.out.println(person.getFirstName()));
+        }
+    }
+
+    /***
+     * created displayPeopleByRegion method display person by region
+     * @param addressBookMap - passing hashmap
+     */
+    public void displayPeopleByRegion(HashMap<String, ArrayList<Person>> addressBookMap) {
+        List<Person> personList;
+        for (String name : addressBookMap.keySet()) {
+            System.out.println("People residing in : " + name);
+            personList = addressBookMap.get(name);
+            for (Person person : personList) {
+                System.out.println(person);
+            }
+        }
+    }
+
+    /***
+     * created countPeopleByRegion method to display number of person in the city or state
+     * @param listToDisplay - passing hash map
+     */
+    public void countPeopleByRegion(HashMap<String, ArrayList<Person>> listToDisplay) {
+        System.out.print("Enter the name of the region : ");
+        String regionName = sc.next();
+        long countPeople = listToDisplay.values().stream()
+                .map(region -> region.stream().filter(person -> person.getState().equals(regionName)
+                                                      || person.getCity().equals(regionName)))
+                .count();
+        System.out.println("Number of People residing in " + regionName +" are : " + countPeople);
+    }
+
+    /***
+     * created sortAddressBook method sort person by name
+     */
+    public void sortAddressBook() {
+        for (String i : multipleAddressBookMap.keySet()) {
+            Map<String, Person> con = multipleAddressBookMap.get(i).multipleAddressBook.personMap;
+            List<Person> sorted = con.values().stream()
+                                              .sorted((firstPerson, secondPerson) -> firstPerson.getFirstName().compareTo(secondPerson.getFirstName()))
+                                              .collect(Collectors.toList());
+            System.out.println("------ Sorted Address Book ------");
+            Iterator iterator = sorted.iterator();
+            while (iterator.hasNext()) {
+                System.out.println(iterator.next());
+                System.out.println();
+            }
+        }
+    }
+}
